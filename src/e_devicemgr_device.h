c54c1b831a4fbea5f76729c60c1bb95793679be7
JengHyun Kang <jhyuni.kang@samsung.com>
2016-02-11 20:35:35 +0900

    device: Add ttrace
    Change-Id: I3e1413ac971dc8afafff49567f46c430908a3de2

--- src/e_devicemgr_device.h
+++ src/e_devicemgr_device.h
@@ -7,6 +7,16 @@
 #include "e_comp_wl.h"
 #include <wayland-server.h>
 
+#ifdef ENABLE_TTRACE
+#include <ttrace.h>
+
+#define TRACE_BEGIN(NAME) traceBegin(TTRACE_TAG_INPUT, "INPUT:DEVMGR:"#NAME)
+#define TRACE_END() traceEnd(TTRACE_TAG_INPUT)
+#else
+#define TRACE_BEGIN(NAME)
+#define TRACE_END()
+#endif
+
 typedef struct _e_devicemgr_input_devmgr_data e_devicemgr_input_devmgr_data;
 
 struct _e_devicemgr_input_devmgr_data
