41e9301964b13dad3e6ab6d2eec1dc4ff66703bb
JengHyun Kang <jhyuni.kang@samsung.com>
2016-04-04 15:32:07 +0900

    input: Add a mouse button remap function
    Change-Id: Ic9b3005e91457da877f66ae549816a364bad5a39

--- src/e_devicemgr_input.c
+++ src/e_devicemgr_input.c
@@ -2,10 +2,12 @@
 #include "e_mod_main.h"
 #include "e_devicemgr_privates.h"
 #include "e_devicemgr_input.h"
+#include "e_devicemgr_device.h"
 
 #include <Ecore_Drm.h>
 static Eina_List *handlers = NULL;
 static Eina_Bool remapped = EINA_FALSE;
+static Ecore_Event_Filter *ev_filter = NULL;
 
 static Eina_Bool
 _cb_input_dev_add(void *data EINA_UNUSED, int type EINA_UNUSED, void *event)
@@ -81,11 +83,107 @@ _cb_input_dev_del(void *data EINA_UNUSED, int type EINA_UNUSED, void *event)
    return ECORE_CALLBACK_PASS_ON;
 }
 
+static void
+_e_devicemgr_input_keyevent_free(void *data EINA_UNUSED, void *ev)
+{
+   Ecore_Event_Key *e = ev;
+
+   eina_stringshare_del(e->keyname);
+   eina_stringshare_del(e->key);
+   eina_stringshare_del(e->compose);
+   eina_stringshare_del(e->dev_name);
+
+   E_FREE(e);
+}
+
+static Eina_Bool
+_e_devicemgr_input_pointer_mouse_remap(int type, void *event)
+{
+   Ecore_Event_Key *ev_key;
+   Ecore_Event_Mouse_Button *ev;
+
+   if (type == ECORE_EVENT_MOUSE_MOVE) return ECORE_CALLBACK_PASS_ON;
+   EINA_SAFETY_ON_NULL_RETURN_VAL(e_comp_wl->xkb.keymap, ECORE_CALLBACK_PASS_ON);
+
+   ev = (Ecore_Event_Mouse_Button *)event;
+   EINA_SAFETY_ON_NULL_RETURN_VAL(ev, ECORE_CALLBACK_PASS_ON);
+
+   if (ev->buttons != 3) return ECORE_CALLBACK_PASS_ON;
+
+   ev_key = E_NEW(Ecore_Event_Key, 1);
+   EINA_SAFETY_ON_NULL_RETURN_VAL(ev_key, ECORE_CALLBACK_PASS_ON);
+
+   ev_key->key = (char *)eina_stringshare_add("XF86Back");
+   ev_key->keyname = (char *)eina_stringshare_add(ev_key->key);
+   ev_key->compose = (char *)eina_stringshare_add(ev_key->key);
+   ev_key->timestamp = (int)(ecore_time_get()*1000);
+   ev_key->same_screen = 1;
+   ev_key->keycode = E_DEVICEMGR_INPUT_BACK_KEYCODE;
+
+   if (type == ECORE_EVENT_MOUSE_BUTTON_DOWN)
+     ecore_event_add(ECORE_EVENT_KEY_DOWN, ev_key, _e_devicemgr_input_keyevent_free, NULL);
+   else if (type == ECORE_EVENT_MOUSE_BUTTON_UP)
+     ecore_event_add(ECORE_EVENT_KEY_UP, ev_key, _e_devicemgr_input_keyevent_free, NULL);
+
+   return ECORE_CALLBACK_DONE;
+}
+
+static Eina_Bool
+_e_devicemgr_input_pointer_process(int type, void *event)
+{
+   Eina_Bool res = ECORE_CALLBACK_PASS_ON;
+
+   res = e_devicemgr_block_check_pointer(type, event);
+   if (res == ECORE_CALLBACK_DONE) return res;
+
+   if (E_DEVICEMGR_INPUT_MOUSE_REMAP_ENABLED)
+     res = _e_devicemgr_input_pointer_mouse_remap(type, event);
+
+   return res;
+}
+
+static Eina_Bool
+_e_devicemgr_input_keyboard_process(int type, void *event)
+{
+   Eina_Bool res = ECORE_CALLBACK_PASS_ON;
+
+   res = e_devicemgr_block_check_keyboard(type, event);
+
+   return res;
+}
+
+static Eina_Bool
+_e_devicemgr_event_filter(void *data, void *loop_data EINA_UNUSED, int type, void *event)
+{
+   (void) data;
+
+   if (ECORE_EVENT_KEY_DOWN == type || ECORE_EVENT_KEY_UP == type)
+     {
+        return _e_devicemgr_input_keyboard_process(type, event);
+     }
+   else if (ECORE_EVENT_MOUSE_BUTTON_DOWN == type ||
+           ECORE_EVENT_MOUSE_BUTTON_UP == type ||
+           ECORE_EVENT_MOUSE_MOVE == type)
+     {
+        return _e_devicemgr_input_pointer_process(type, event);
+     }
+   else if (ECORE_EVENT_MOUSE_WHEEL == type)
+     {
+        return e_devicemgr_detent_check(type, event);
+     }
+
+   return ECORE_CALLBACK_PASS_ON;
+}
+
+
 int
 e_devicemgr_input_init(void)
 {
    E_LIST_HANDLER_APPEND(handlers, ECORE_EVENT_DEVICE_ADD, _cb_input_dev_add, NULL);
    E_LIST_HANDLER_APPEND(handlers, ECORE_EVENT_DEVICE_DEL, _cb_input_dev_del, NULL);
+
+   /* add event filter for blocking events */
+   ev_filter = ecore_event_filter_add(NULL, _e_devicemgr_event_filter, NULL, NULL);
 
    return 1;
 }
@@ -94,4 +192,7 @@ void
 e_devicemgr_input_fini(void)
 {
    E_FREE_LIST(handlers, ecore_event_handler_del);
+
+   /* remove existing event filter */
+   ecore_event_filter_del(ev_filter);
 }
--- src/e_devicemgr_input.h
+++ src/e_devicemgr_input.h
@@ -5,6 +5,11 @@
 
 #include "e.h"
 
+/* FIXME: Optional values will be set in configuration file. */
+/* FIXME: keycode will be get using keymap */
+#define E_DEVICEMGR_INPUT_BACK_KEYCODE 166
+#define E_DEVICEMGR_INPUT_MOUSE_REMAP_ENABLED EINA_TRUE
+
 int e_devicemgr_input_init(void);
 void e_devicemgr_input_fini(void);
 
